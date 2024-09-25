b2074437515439ffd2d44aa9e06490ba8c345c76
btcthirst <btcthirst@gmail.com>
2024-09-26 01:07:50 +0300

    added: simple writer

--- excel-reader/src/main.rs
+++ excel-reader/src/main.rs
@@ -1,11 +1,15 @@
-use calamine::{deserialize_as_f64_or_none, open_workbook, RangeDeserializerBuilder, Reader, Xlsx};
+use calamine::{deserialize_as_f64_or_none, deserialize_as_i64_or_none, open_workbook, RangeDeserializerBuilder, Reader, Xlsx};
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize,Debug)]
 struct Record {
-    metric: String,
+    name: String,
+    #[serde(deserialize_with = "deserialize_as_i64_or_none")]
+    value: Option<i64>,
     #[serde(deserialize_with = "deserialize_as_f64_or_none")]
-    value: Option<f64>,
+    price: Option<f64>,
+    #[serde(deserialize_with = "deserialize_as_f64_or_none")]
+    sum: Option<f64>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -14,15 +18,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut excel: Xlsx<_> = open_workbook(path)?;
 
     let range = excel
-        .worksheet_range("Sheet1")
-        .map_err(|_| calamine::Error::Msg("Cannot find Sheet1"))?;
+        .worksheet_range("Хортиця")
+        .map_err(|_| calamine::Error::Msg("Cannot find Хортиця"))?;
 
     let iter_records =
-        RangeDeserializerBuilder::with_headers(&["metric", "value"]).from_range(&range)?;
+        RangeDeserializerBuilder::with_headers(&["name", "value", "price", "sum"]).from_range(&range)?;
 
     for result in iter_records {
         let record: Record = result?;
-        println!("\n metric={:?}, value={:?}", record.metric, record.value);
+        println!("\n row={:#?}", record);
     }
 
     Ok(())
