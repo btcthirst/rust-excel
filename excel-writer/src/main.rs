c04c24951fe523e523f7fbec8436bca9a00a7816
btcthirst <btcthirst@gmail.com>
2024-09-28 18:39:26 +0300

    updated: table data

--- excel-writer/src/main.rs
+++ excel-writer/src/main.rs
@@ -1,6 +1,37 @@
 use rust_xlsxwriter::*;
 
+#[derive(Debug)]
+struct Somename {
+    name: String,
+    value: i64,
+    price_in: f64,
+    price_out: f64
+}
 fn main() -> Result<(), XlsxError> {
+
+    let mut data:Vec<Somename> = Vec::new();
+    data.push(Somename{
+            name: String::from("пиво ЛЬВІВСЬКЕ світле 0.5 л ск/б"),
+            value: 15,
+            price_in: 30.1,
+            price_out: 35.0
+        });
+        
+    data.push(Somename{
+            name: String::from("пиво ЛЬВІВСЬКЕ 1715 1 л "),
+            value: 10,
+            price_in: 50.15,
+            price_out: 65.0
+        });
+    data.push(Somename{
+            name: String::from("пиво ЛЬВІВСЬКЕ оксамит 0.5 л з/б"),
+            value: 9,
+            price_in: 30.35,
+            price_out: 36.0
+        });
+   
+        
+
     // Create a new Excel file object.
     let mut workbook = Workbook::new();
 
@@ -14,10 +45,31 @@ fn main() -> Result<(), XlsxError> {
 
     // Add a worksheet to the workbook.
     let worksheet = workbook.add_worksheet();
+    // add worksheet name
+    let _ = worksheet.set_name("Пивний двір");
 
     // Set the column width for clarity.
-    worksheet.set_column_width(0, 22)?;
-
+    worksheet.set_column_width(0, 35)?;
+    let mut row = 0;
+    worksheet.write_with_format(row, 0, "назва",&bold_format)?;
+    worksheet.write_with_format(row, 1, "кількість",&bold_format)?;
+    worksheet.write_with_format(row, 2, "ціна приходу",&bold_format)?;
+    worksheet.write_with_format(row, 3, "сума",&bold_format)?;
+    worksheet.write_with_format(row, 4, "ціна продажу",&bold_format)?;
+    row +=1;
+    
+    for el in data {
+            worksheet.write(row, 0, el.name)?;
+            worksheet.write_with_format(row, 1, el.value,&decimal_format)?;
+            worksheet.write_with_format(row, 2, el.price_in,&decimal_format)?;
+            let formula = format!("= B{}*C{}",row+1,row+1);
+            worksheet.write_formula_with_format(row, 3, Formula::new(&formula),&decimal_format)?;
+            worksheet.write_with_format(row, 4, el.price_out,&decimal_format)?;
+            row +=1;
+    }
+            
+       
+    /*
     // Write a string without formatting.
     worksheet.write(0, 0, "Hello")?;
 
@@ -48,9 +100,10 @@ fn main() -> Result<(), XlsxError> {
     // Insert an image.
     //let image = Image::new("examples/rust_logo.png")?;
     //worksheet.insert_image(1, 2, &image)?;
-
+    */
     // Save the file to disk.
     workbook.save("../excel-reader/testdata/test1.xlsx")?;
 
     Ok(())
 }
+
