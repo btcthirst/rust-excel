899ba21d28e6c8e55a2b39f4582107cc23242802
btcthirst <btcthirst@gmail.com>
2024-11-03 18:16:40 +0200

    updated

--- excel-reader/src/main.rs
+++ excel-reader/src/main.rs
@@ -1,34 +1,59 @@
 use calamine::{deserialize_as_f64_or_none, deserialize_as_i64_or_none, open_workbook, RangeDeserializerBuilder, Reader, Xlsx};
 use serde::Deserialize;
 
-#[derive(Deserialize,Debug)]
+#[derive(Deserialize,Debug,Clone)]
 struct Record {
+    #[serde(default)]
     name: String,
-    #[serde(deserialize_with = "deserialize_as_i64_or_none")]
-    value: Option<i64>,
-    #[serde(deserialize_with = "deserialize_as_f64_or_none")]
-    price: Option<f64>,
-    #[serde(deserialize_with = "deserialize_as_f64_or_none")]
-    sum: Option<f64>,
+    #[serde(default)]
+    unit_of_meas: String,    
+    #[serde(default,deserialize_with = "deserialize_as_f64_or_none")]
+    price_in: Option<f64>,
+    #[serde(default,deserialize_with = "deserialize_as_f64_or_none")]
+    sum_in: Option<f64>,
+    #[serde(default,deserialize_with = "deserialize_as_f64_or_none")]
+    price_out: Option<f64>,
+    #[serde(default,deserialize_with = "deserialize_as_i64_or_none")]
+    percent: Option<i64>,   
+    #[serde(default,deserialize_with = "deserialize_as_f64_or_none")]
+    sum_out: Option<f64>,
+    #[serde(default,deserialize_with = "deserialize_as_f64_or_none")]
+    value: Option<f64>,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = format!("{}/testdata/test.xlsx", env!("CARGO_MANIFEST_DIR"));
-    print!("path: {}",&path);
+fn main()  {
+    let store: Vec<Record>;
+    let path = format!("../../test_data/test.xlsx");
+    println!("path: {}",&path);
+    
+    let mut res = read_excel(path).expect("Vec rec");
+    res.retain(|r| r.name != ""); // deleted record with name equal to empty
+    res.retain(|r| !(r.price_in == None && r.price_out == None));
+    store = res;
+    
+
+   println!("{:#?}, lenth {:?}",store, store.len())
+}
+
+fn read_excel(path: String) -> Result<Vec<Record>, Box<dyn std::error::Error>>{
+    let mut res: Vec<Record> = Vec::new();
     let mut excel: Xlsx<_> = open_workbook(path)?;
 
+    let sheet_names = excel.sheet_names();
+    println!("{:?}",sheet_names);
     let range = excel
-        .worksheet_range("Хортиця")
-        .map_err(|_| calamine::Error::Msg("Cannot find Хортиця"))?;
+        .worksheet_range(&sheet_names[0])
+        .map_err(|_| calamine::Error::Msg("Cannot find sheet_names[0]"))?;
 
     let iter_records =
-        RangeDeserializerBuilder::with_headers(&["name", "value", "price", "sum"]).from_range(&range)?;
+        RangeDeserializerBuilder::with_headers(&["name", "unit_of_meas", "price_in", "sum_in","price_out", "percent", "sum_out", "value"]).from_range(&range)?;
 
     for result in iter_records {
-        let record: Record = result?;
-        println!("\n row={:#?}", record);
+        //let rec: Record = result?;
+        
+        res.push(result?);
+        
     }
 
-    Ok(())
+    Ok(res)
 }
-
